69a0c8e2bbb58462e3920f75e599d84cbcb1322e
Inesculent <122597009+Inesculent@users.noreply.github.com>
2024-08-29 23:35:16 +0000

    Changed embed

--- getembedding.py
+++ getembedding.py
@@ -1,6 +1,6 @@
 #Changed from Bedrock to Ollama cause of some error, will have to figure out later
 #from langchain_community.embeddings.ollama import OllamaEmbeddings
-from chromadb.utils.embedding_functions import DefaultEmbeddingFunction
+from chromadb.utils import embedding_functions
 from langchain_community.embeddings.bedrock import BedrockEmbeddings
 from langchain_openai.embeddings import OpenAIEmbeddings
 
@@ -8,7 +8,7 @@ def get_embedding_function():
     #embeddings = BedrockEmbeddings(client = any, credentials_profile_name="default", region_name="us-east-1")
     #embeddings = OllamaEmbeddings(model="nomic-embed-text")
     #embeddings = OpenAIEmbeddings(openai_api_key='sk-3BxHhdzWnm_pT-jC_icP0616Mw7kNFUQQGC4a7PX51T3BlbkFJqQR00moyfRknwUhq1U4Rzcgdio3SZqqd6Cr4eZHzsA')
-    embeddings = DefaultEmbeddingFunction
+    embeddings = embedding_function()
 
     return embeddings
 
