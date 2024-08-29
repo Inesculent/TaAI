0572e836331acf6178a6ec6a175619e9855d2d06
Inesculent <122597009+Inesculent@users.noreply.github.com>
2024-08-29 22:18:56 +0000

    Added protobuf

--- main.py
+++ main.py
@@ -97,7 +97,9 @@ def load_documents(save_folder):
 def add_to_chroma(chunks: list[Document]):
   # Load the existing database.
     st.write("a")
-    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=get_embedding_function())
+    db = Chroma(
+        persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
+    )
     st.write("b")
     # Calculate Page IDs.
     chunks_with_ids = calculate_chunk_ids(chunks)
