9023f8dccefb6f7efcdf974cc40bf78d49a1e4cb
Inesculent <122597009+Inesculent@users.noreply.github.com>
2024-08-29 00:49:04 +0000

    Changed replit.nix

--- replit.nix
+++ replit.nix
@@ -1,11 +1,6 @@
-{ pkgs }: {
+{pkgs}: {
   deps = [
-<<<<<<< HEAD
     pkgs.fetchutils
     pkgs.bash
-=======
-    pkgs.python310 
-    pkgs.python310Packages.pip
->>>>>>> 28e45e73dc0208a9f8df51c4322c45e08ccca560
   ];
 }
