772e0083baf4a915c3eab913e1cbd54dd30b1cbe
Inesculent <122597009+Inesculent@users.noreply.github.com>
2024-08-29 23:15:09 +0000

    Changed API key

--- getembedding.py
+++ getembedding.py
@@ -6,7 +6,7 @@ from langchain_openai.embeddings import OpenAIEmbeddings
 def get_embedding_function():
     #embeddings = BedrockEmbeddings(client = any, credentials_profile_name="default", region_name="us-east-1")
     #embeddings = OllamaEmbeddings(model="nomic-embed-text")
-    embeddings = OpenAIEmbeddings(openai_api_key='sk-hAsV9Zqp7EQVY3KW-hb0ll6KGypZxouf4x08qTaIr4T3BlbkFJgOulnwUGR6wQDM3n_hVp1NrmuN04GLtIvfkEtVeesA')
+    embeddings = OpenAIEmbeddings(openai_api_key='sk-3BxHhdzWnm_pT-jC_icP0616Mw7kNFUQQGC4a7PX51T3BlbkFJqQR00moyfRknwUhq1U4Rzcgdio3SZqqd6Cr4eZHzsA')
     return embeddings
 
 
