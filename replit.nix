28e45e73dc0208a9f8df51c4322c45e08ccca560
Inesculent <122597009+Inesculent@users.noreply.github.com>
2024-08-28 20:15:49 -0400

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,6 +1,6 @@
 { pkgs }: {
   deps = [
-    pkgs.python310   # Change this to the version you want, e.g., pkgs.python311 for Python 3.11
+    pkgs.python310 
     pkgs.python310Packages.pip
   ];
 }
