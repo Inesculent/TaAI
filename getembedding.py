38320f3d8e98aea1520ad77d86a947b0330894d6
Inesculent <122597009+Inesculent@users.noreply.github.com>
2024-08-29 23:36:41 +0000

    Changed embed

--- getembedding.py
+++ getembedding.py
@@ -8,8 +8,7 @@ def get_embedding_function():
     #embeddings = BedrockEmbeddings(client = any, credentials_profile_name="default", region_name="us-east-1")
     #embeddings = OllamaEmbeddings(model="nomic-embed-text")
     #embeddings = OpenAIEmbeddings(openai_api_key='sk-3BxHhdzWnm_pT-jC_icP0616Mw7kNFUQQGC4a7PX51T3BlbkFJqQR00moyfRknwUhq1U4Rzcgdio3SZqqd6Cr4eZHzsA')
-    embeddings = embedding_function()
-
+    embeddings = embedding_functions.DefaultEmbeddingFunction()
     return embeddings
 
 
