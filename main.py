e507324f12200473ef425b8e049ab68758e9680e
Inesculent <122597009+Inesculent@users.noreply.github.com>
2024-08-31 08:09:20 +0000

    So gross

--- main.py
+++ main.py
@@ -164,8 +164,7 @@ def clear_database(documents_list):
         for i in documents_list:
             st.code(i.strip('"\''))
             
-        deleteFile = st.text_input("Enter the file that you want to delete").strip()
-        if deleteFile:
+        if st.text_input("Enter the file that you want to delete").strip():
             db.delete(
                 where={"source": deleteFile}
             )
