a3c4cc0821392ff8a984925a92babf1ae6839817
Inesculent <122597009+Inesculent@users.noreply.github.com>
2024-08-31 07:35:52 +0000

    Please work

--- main.py
+++ main.py
@@ -30,18 +30,18 @@ def main():
         st.write("Successfully uploaded a PDF file.")
         save_folder = './pdfs'
 
-
-
+       
+        
         if not os.path.exists(save_folder):
           os.mkdir(save_folder)
           print("Folder %s created!" % save_folder)
         else:
           print("Folder %s already exists" % save_folder)
-
+        
         save_path = Path(save_folder, documents.name)
-        if (save_path not in documents_list):
-            documents_list.append(save_path)
-
+        if (("pdfs/" + documents.name) not in documents_list):
+            documents_list.append("pdfs/" + documents.name)
+        
         with open(save_path, mode='wb') as w:
             w.write(documents.getvalue())
 
@@ -49,7 +49,7 @@ def main():
             st.success(f'File {documents.name} is successfully saved in {save_folder}')
         else:
             st.error(f'Error saving file {documents.name}.')
-
+            
 
         documents = load_documents(save_folder)
         if (documents):
@@ -66,9 +66,9 @@ def main():
     button = st.button("Clear database")
     if button:
         print("✨ Clearing Database")
-        clear_database()
-
-
+        clear_database(documents_list)
+            
+        
    ##    documents = load_documents()
 
 def load_documents(save_folder):
@@ -79,14 +79,14 @@ def load_documents(save_folder):
     return document_loader.load()
 
 
-
+   
 def add_to_chroma(chunks: list[Document]):
   # Load the existing database.
     st.write("a")
     db = Chroma(
         persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
     )
-
+    
     st.write("b")
     # Calculate Page IDs.
     chunks_with_ids = calculate_chunk_ids(chunks)
@@ -96,13 +96,13 @@ def add_to_chroma(chunks: list[Document]):
     st.write(os.getenv("OPENAI_API_KEY"))
     existing_ids = set(existing_items["ids"])
     print(f"Number of existing documents in DB: {len(existing_ids)}")
-
+    
     # Only add documents that don't exist in the DB.
     new_chunks = []
     for chunk in chunks_with_ids:
         if chunk.metadata["id"] not in existing_ids:
           new_chunks.append(chunk)
-
+    
     if len(new_chunks):
         print(f"Adding new documents: {len(new_chunks)}")
         new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
@@ -110,33 +110,33 @@ def add_to_chroma(chunks: list[Document]):
         db.persist()
     else:
         print("No new documents to add")
-
+        
 
 def calculate_chunk_ids(chunks):
 
 # Page Source : Page Number : Chunk Index
-
+    
     last_page_id = None
     current_chunk_index = 0
-
+    
     for chunk in chunks:
         source = chunk.metadata.get("source")
         page = chunk.metadata.get("page")
         current_page_id = f"{source}:{page}"
-
+    
         # If the page ID is the same as the last one, increment the index.
         if current_page_id == last_page_id:
             current_chunk_index += 1
         else:
             current_chunk_index = 0
-
+    
         # Calculate the chunk ID.
         chunk_id = f"{current_page_id}:{current_chunk_index}"
         last_page_id = current_page_id
-
+    
         # Add it to the page meta-data.
         chunk.metadata["id"] = chunk_id
-
+    
     return chunks
 
 
@@ -149,21 +149,26 @@ def split_documents(documents: list[Document]):
     )
     return text_splitter.split_documents(documents)
 
-def clear_database():
+def clear_database(documents_list):
     if os.path.exists(CHROMA_PATH):
         db = Chroma(
             persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
         )
 
         deleteFile = st.text_input("Enter the file that you want to delete")
-
-        st.write(db.get(include=[]))
+        
+        #st.write(db.get(include=[]))
+        
+        #st.write(documents_list)
+        for i in documents_list:
+            st.code(i.strip('"\''))
 
         if deleteFile:
             db.delete(
                 where={"source": deleteFile}
             )
             db.persist()
+            documents_list.remove(deleteFile)
             st.write(f"Sucessfully cleared database of file: {deleteFile}")
             st.write(db.get(include=[]))
     else:
@@ -171,3 +176,6 @@ def clear_database():
 
 if __name__ == "__main__":
     main()
+
+
+
